5d208fd05bb144c91e47cd2259c94c95001bb7b9
Ryunosuke SATO <tricknotes.rs@gmail.com>
2013-06-15 21:53:11 +0900

    Add methods for analize bottle message

--- app/models/bottle.rb
+++ app/models/bottle.rb
@@ -1,6 +1,31 @@
 class Bottle < ActiveRecord::Base
   belongs_to :owner, class_name: "User"
 
+  class << self
+    def analize_words
+      words = Hash.new(0)
+
+      model = MeCab::Model.new()
+      tagger = model.createTagger
+      all.each do |bottle|
+        node = tagger.parseToNode(bottle.message)
+
+        while node
+          feature = node.feature
+
+          # XXX Bad code...
+          if feature['名詞']
+            words[node.surface] += 1
+          end
+
+          node = node.next
+        end
+      end
+
+      words
+    end
+  end
+
   def deliver!(date = DateTime.now)
     self.delivered_at = date
     save!
