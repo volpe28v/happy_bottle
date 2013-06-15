a645e792b5b55006a1c012445a65c08531499ac8
Ryunosuke SATO <tricknotes.rs@gmail.com>
2013-06-16 00:52:15 +0900

    config for heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,7 +20,7 @@ HappyBottle::Application.configure do
   # config.action_dispatch.rack_cache = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this).
-  config.serve_static_assets = false
+  config.serve_static_assets = true
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
@@ -64,6 +64,18 @@ HappyBottle::Application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
+
+  config.action_mailer.default_url_options = { :host => "happy-bottle.herokuapp.com", :port => 80 }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address => "smtp.gmail.com",
+    :port => 587,
+    :domain => 'gmail.com',
+    :user_name => ENV['MAIL_ADDR'],
+    :password => ENV['MAIL_PASSWORD'],
+    :authentication => 'plain',
+    :enable_starttls_auto => true,
+  }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation can not be found).
