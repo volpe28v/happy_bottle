6adaf5b5bc4e92dd3449554c2db41e19acc801ac
Ryunosuke SATO <tricknotes.rs@gmail.com>
2013-06-15 11:45:01 +0900

    Use encrypted password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,17 @@
 class User < ActiveRecord::Base
   attr_accessor :password
 
+  before_save do
+    self.hashed_password = self.class.encrypt(self.password) if self.password.present?
+  end
+
   class << self
     def find_by_email_and_password(email, password)
-      User.where(email: email, hashed_password: password).first
+      User.where(email: email, hashed_password: encrypt(password)).first
+    end
+
+    def encrypt(password)
+      Digest::SHA1.hexdigest(password)
     end
   end
 end
