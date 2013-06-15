f165dbce1713ac6146d94b22eddcf237fe5bfc6b
naoki kodama <naoki.koda@gmail.com>
2013-06-15 11:15:21 +0900

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HappyBottle::Application.config.secret_key_base = '191fc18415a6c9c321b31e656b3d2384fc7edc32d57bfd3548b09c058366919080ecfe39a4731f5a553d6af97c1c5aec276c028e1f653bd6788758341277e4b8'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+HappyBottle::Application.config.session_store :cookie_store, key: '_happy_bottle_session'
