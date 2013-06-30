d1df186fe297354a54031b4a2f22575056a115e9
Ryunosuke SATO <tricknotes.rs@gmail.com>
2013-06-30 23:27:15 +0900

    Fix invitation mail's sender

--- app/mailers/invitation_mailer.rb
+++ app/mailers/invitation_mailer.rb
@@ -1,5 +1,5 @@
 class InvitationMailer < ActionMailer::Base
-  default from: "from@example.com"
+  default from: "HappyBottle<#{ENV['MAIL_ADDR']}>"
 
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
