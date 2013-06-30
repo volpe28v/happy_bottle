4cd34dc4522cab861cb23c8c4d1f725e4adf639d
Ryunosuke SATO <tricknotes.rs@gmail.com>
2013-06-30 23:18:46 +0900

    Use `truncation` instead of `transaction`
    The features using poltergeist couldn't touch
    testing data through thread.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -37,7 +37,7 @@ RSpec.configure do |config|
   config.include FactoryGirl::Syntax::Methods
 
   config.before(:suite) do
-    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.strategy = :truncation
     DatabaseCleaner.clean_with :truncation
   end
 
