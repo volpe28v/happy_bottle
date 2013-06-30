55f01bef4675ef430f47b0ffe712d6bc6cb4dad3
naoki kodama <naoki.koda@gmail.com>
2013-06-30 23:27:27 +0900

    fix typo

--- spec/features/display_piece_of_bottle_spec.rb
+++ spec/features/display_piece_of_bottle_spec.rb
@@ -11,7 +11,7 @@ feature '瓶の欠片を表示する', js: true do
     scenario '瓶の欠片を表示することができる' do
       within '#peace_of_bottle' do
         save_screenshot '/tmp/hi.png'
-        page.should have_content('オム')
+        page.should have_content('オムライス')
       end
     end
   end
