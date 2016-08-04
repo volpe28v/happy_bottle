309828f5211613723f8fd4dc49787e9ac80e30bb
naoki kodama <naoki.koda@gmail.com>
2016-08-04 13:25:28 +0900

    add send grid settings.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -32,13 +32,13 @@ HappyBottle::Application.configure do
   if ENV['MAIL_ADDR'] && ENV['MAIL_PASSWORD']
     config.action_mailer.delivery_method = :smtp
     config.action_mailer.smtp_settings = {
-      :address => "smtp.gmail.com",
-      :port => 587,
-      :domain => 'gmail.com',
-      :user_name => ENV['MAIL_ADDR'],
-      :password => ENV['MAIL_PASSWORD'],
-      :authentication => 'plain',
-      :enable_starttls_auto => true,
+      :address        => 'smtp.sendgrid.net',
+      :port           => '587',
+      :authentication => :plain,
+      :user_name      => ENV['SENDGRID_USERNAME'],
+      :password       => ENV['SENDGRID_PASSWORD'],
+      :domain         => 'heroku.com',
+      :enable_starttls_auto => true
     }
   else
     config.action_mailer.delivery_method = :letter_opener
